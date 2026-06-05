37a9bd6f1306821d31b0bb5621e0e4aa1fda4a2f
IrwinUM <irwinum6@gmail.com>
2026-06-05 10:33:16 +0800

    Add charging session entity

---
+++ src/Domain/Entities/ChargingSession.cs
@@ -0,0 +1,14 @@
+namespace EVChargingBackend.Domain.Entities;
+
+public class ChargingSession
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public decimal EnergyKwh { get; set; }
+
+    public decimal TariffRatePerKwh { get; set; }
+
+    public int Status { get; set; }
+}
